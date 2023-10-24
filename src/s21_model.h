04eb2a0036bf03a32334d1fbedb58d99457deb9c
elidacon <elidacon@student.21-school.ru>
2023-10-24 23:37:41 +0500

    Refactor s21_model.h

--- src/s21_model.h
+++ src/s21_model.h
@@ -7,31 +7,6 @@
 #include <unordered_map>
 #include <variant>
 
-enum class TokenType : char {
-  kNone,
-  kNumber,
-  kOpenParenthesis,
-  kCloseParenthesis,
-  kDiv,
-  kMul,
-  kExp,
-  kPlus,
-  kMinus,
-  kMod,
-  kCos,
-  kSin,
-  kTan,
-  kAcos,
-  kAsin,
-  kAtan,
-  kSqrt,
-  kLn,
-  kLog,
-  kX
-};
-
-using Token = std::variant<double, TokenType>;
-
 class Model {
  public:
   bool ParseExpression(const std::string& expression) {
@@ -44,12 +19,11 @@ class Model {
     auto expression_end = expression.end();
     auto it = expression.begin();
     while (it != expression_end) {
-      auto c = *it;
       curr_token_type = GetTokenType(it, expression_end);
       if (!IsValidToken(curr_token_type, prev_token_type)) {
         return false;
       }
-      if (c == ')') {
+      if (curr_token_type == TokenType::kCloseParenthesis) {
         // A right parenthesis:
         // While the token at the top of the stack is not a left parenthesis,
         // pop the token-operators from the stack into the output queue.
@@ -82,7 +56,6 @@ class Model {
             }
           }
         }
-        it++;
       } else if (curr_token_type == TokenType::kNumber) {
         curr_token_type = TokenType::kNumber;
         auto i = it - expression.begin();
@@ -99,11 +72,10 @@ class Model {
         it += end - i;
       } else if (curr_token_type == TokenType::kX) {
         rpn_queue_.push(curr_token_type);
-        it++;
-      } else if (IsOperator(curr_token_type)) {
+      } else if (IsBinaryOperator(curr_token_type)) {
         if ((prev_token_type == TokenType::kNone ||
              prev_token_type == TokenType::kOpenParenthesis) &&
-            IsUnaryOperator(curr_token_type)) {
+            IsPlusMinus(curr_token_type)) {
           rpn_queue_.push(0.0);
         }
 
@@ -115,10 +87,8 @@ class Model {
           rpn_queue_.push(top);
         }
         op_stack.push(curr_token_type);
-        if (curr_token_type != TokenType::kMod) it++;
       } else {
         op_stack.push(curr_token_type);
-        if (curr_token_type == TokenType::kOpenParenthesis) it++;
       }
       prev_token_type = curr_token_type;
     }
@@ -155,13 +125,13 @@ class Model {
       } else if (auto op = std::get_if<TokenType>(&token)) {
         if (*op == TokenType::kX) {
           eval_stack.push(x);
-        } else if (*op >= TokenType::kCos) {
+        } else if (IsFunction(*op)) {
           if (eval_stack.empty()) {
             throw std::invalid_argument("Not enough arguments for function");
           }
           double arg = eval_stack.top();
           eval_stack.pop();
-          eval_stack.push(unary_functions[*op](arg));
+          eval_stack.push(unary_functions_map_[*op](arg));
         } else {
           if (eval_stack.size() < 2) {
             throw std::invalid_argument("Not enough arguments for operator");
@@ -170,7 +140,7 @@ class Model {
           eval_stack.pop();
           double arg1 = eval_stack.top();
           eval_stack.pop();
-          eval_stack.push(binary_functions[*op](arg1, arg2));
+          eval_stack.push(binary_functions_map_[*op](arg1, arg2));
         }
       }
     }
@@ -183,12 +153,34 @@ class Model {
   }
 
  private:
+  enum class TokenType : char {
+    kNone,
+    kNumber,
+    kX,
+    kOpenParenthesis,
+    kCloseParenthesis,
+    kDiv,
+    kMul,
+    kExp,
+    kPlus,
+    kMinus,
+    kMod,
+    kSin,
+    kCos,
+    kTan,
+    kAsin,
+    kAcos,
+    kAtan,
+    kSqrt,
+    kLn,
+    kLog
+  };
+
+  using Token = std::variant<double, TokenType>;
+
   static double Add(double a, double b) { return a + b; }
-
   static double Subtract(double a, double b) { return a - b; }
-
   static double Multiply(double a, double b) { return a * b; }
-
   static double Divide(double a, double b) {
     if (b == 0) {
       throw std::invalid_argument("Division by zero");
@@ -197,18 +189,41 @@ class Model {
   }
 
   std::queue<Token> rpn_queue_;
-  std::unordered_map<TokenType, std::function<double(double)>> unary_functions =
-      {{TokenType::kSin, static_cast<double (*)(double)>(std::sin)},
-       {TokenType::kCos, static_cast<double (*)(double)>(std::cos)},
-       {TokenType::kTan, static_cast<double (*)(double)>(std::tan)},
-       {TokenType::kAsin, static_cast<double (*)(double)>(std::asin)},
-       {TokenType::kAcos, static_cast<double (*)(double)>(std::acos)},
-       {TokenType::kAtan, static_cast<double (*)(double)>(std::atan)},
-       {TokenType::kSqrt, static_cast<double (*)(double)>(std::sqrt)},
-       {TokenType::kLn, static_cast<double (*)(double)>(std::log)},
-       {TokenType::kLog, static_cast<double (*)(double)>(std::log10)}};
-  std::unordered_map<TokenType, std::function<double(double, double)>>
-      binary_functions = {
+
+  static inline std::unordered_map<std::string, TokenType> tokens_map_ = {
+      {"(", TokenType::kOpenParenthesis},
+      {")", TokenType::kCloseParenthesis},
+      {"/", TokenType::kDiv},
+      {"*", TokenType::kMul},
+      {"^", TokenType::kExp},
+      {"+", TokenType::kPlus},
+      {"-", TokenType::kMinus},
+      {"mod", TokenType::kMod},
+      {"cos", TokenType::kCos},
+      {"sin", TokenType::kSin},
+      {"tan", TokenType::kTan},
+      {"acos", TokenType::kAcos},
+      {"asin", TokenType::kAsin},
+      {"atan", TokenType::kAtan},
+      {"sqrt", TokenType::kSqrt},
+      {"ln", TokenType::kLn},
+      {"log", TokenType::kLog},
+      {"x", TokenType::kX}};
+
+  static inline std::unordered_map<TokenType, std::function<double(double)>>
+      unary_functions_map_ = {
+          {TokenType::kSin, static_cast<double (*)(double)>(std::sin)},
+          {TokenType::kCos, static_cast<double (*)(double)>(std::cos)},
+          {TokenType::kTan, static_cast<double (*)(double)>(std::tan)},
+          {TokenType::kAsin, static_cast<double (*)(double)>(std::asin)},
+          {TokenType::kAcos, static_cast<double (*)(double)>(std::acos)},
+          {TokenType::kAtan, static_cast<double (*)(double)>(std::atan)},
+          {TokenType::kSqrt, static_cast<double (*)(double)>(std::sqrt)},
+          {TokenType::kLn, static_cast<double (*)(double)>(std::log)},
+          {TokenType::kLog, static_cast<double (*)(double)>(std::log10)}};
+  static inline std::unordered_map<TokenType,
+                                   std::function<double(double, double)>>
+      binary_functions_map_ = {
           {TokenType::kPlus, Add},
           {TokenType::kMinus, Subtract},
           {TokenType::kMul, Multiply},
@@ -216,24 +231,7 @@ class Model {
           {TokenType::kMod, static_cast<double (*)(double, double)>(std::fmod)},
           {TokenType::kExp, static_cast<double (*)(double, double)>(std::pow)}};
 
-  std::vector<std::string> functions = {"sin",  "cos",  "tan", "asin", "acos",
-                                        "atan", "sqrt", "ln",  "log",  "mod"};
-
-  TokenType StringToTokenType(const std::string& str) {
-    if (str == "sin") return TokenType::kSin;
-    if (str == "cos") return TokenType::kCos;
-    if (str == "tan") return TokenType::kTan;
-    if (str == "asin") return TokenType::kAsin;
-    if (str == "acos") return TokenType::kAcos;
-    if (str == "atan") return TokenType::kAtan;
-    if (str == "sqrt") return TokenType::kSqrt;
-    if (str == "ln") return TokenType::kLn;
-    if (str == "log") return TokenType::kLog;
-    if (str == "mod") return TokenType::kMod;
-    return TokenType::kNone;
-  }
-
-  bool IsOperator(TokenType type) {
+  bool IsBinaryOperator(TokenType type) {
     return type >= TokenType::kDiv && type <= TokenType::kMod;
   }
 
@@ -241,17 +239,15 @@ class Model {
     return type == TokenType::kNumber || type == TokenType::kX;
   }
 
-  bool IsFunction(TokenType type) {
-    return type >= TokenType::kCos && type < TokenType::kX;
-  }
+  bool IsFunction(TokenType type) { return type >= TokenType::kSin; }
 
-  bool IsUnaryOperator(TokenType type) {
+  bool IsPlusMinus(TokenType type) {
     return type == TokenType::kPlus || type == TokenType::kMinus;
   }
 
   int GetPriority(TokenType type) {
     int res = 2;
-    if (IsUnaryOperator(type)) {
+    if (IsPlusMinus(type)) {
       res = 1;
     } else if (type == TokenType::kExp) {
       res = 3;
@@ -267,77 +263,54 @@ class Model {
     TokenType res = TokenType::kNone;
     if (std::isdigit(*it)) {
       res = TokenType::kNumber;
-    } else if (*it == 'x') {
-      res = TokenType::kX;
-    } else if (std::isalpha(*it)) {
-      std::string expression(it, end);
-      auto match = std::find_if(functions.begin(), functions.end(),
-                                [&](const std::string& s) {
-                                  return expression.substr(0, s.size()) == s;
-                                });
-      if (match != functions.end()) {
-        it += match->size();
-        res = StringToTokenType(*match);
-      }
     } else {
-      switch (*it) {
-        case '(':
-          res = TokenType::kOpenParenthesis;
-          break;
-        case ')':
-          res = TokenType::kCloseParenthesis;
-          break;
-        case '/':
-          res = TokenType::kDiv;
-          break;
-        case '*':
-          res = TokenType::kMul;
-          break;
-        case '^':
-          res = TokenType::kExp;
-          break;
-        case '+':
-          res = TokenType::kPlus;
-          break;
-        case '-':
-          res = TokenType::kMinus;
-          break;
-        case 'x':
-          res = TokenType::kX;
-          break;
+      std::string token(1, *it);
+      if (std::isalpha(*it) && *it != 'x') {
+        while (it++ != end && std::isalpha(*it) && *it != 'x') {
+          token += *it;
+        }
+      } else {
+        it++;
+      }
+      auto match = tokens_map_.find(token);
+      if (match != tokens_map_.end()) {
+        res = match->second;
       }
     }
     return res;
   }
 
   bool IsValidToken(TokenType ctt, TokenType ptt) {
+    if (ctt == TokenType::kNone) return false;
+
     bool res = false;
-    if (ctt == TokenType::kNone) {
-      res = false;
-    } else if (IsValue(ctt)) {
-      // A number should be preceded by an operator or an opening parenthesis.
-      res = ptt == TokenType::kNone || IsOperator(ptt) ||
+    if (IsValue(ctt)) {
+      // A number should be preceded by a binary operator or an opening
+      // parenthesis.
+      res = ptt == TokenType::kNone || IsBinaryOperator(ptt) ||
             ptt == TokenType::kOpenParenthesis;
-    } else if (IsOperator(ctt)) {
-      // An operator should be preceded by a number or a close parenthesis.
+    } else if (IsBinaryOperator(ctt)) {
+      // A binary operator should be preceded by a number or a close
+      // parenthesis.
       res = IsValue(ptt) || ptt == TokenType::kCloseParenthesis;
-      if (!res && IsUnaryOperator(ctt)) {
-        // A unary operator can also be the first char or preceded by an
+      if (!res && IsPlusMinus(ctt)) {
+        // An unary operator can also be the first char or preceded by an
         // opening parenthesis
         res = ptt == TokenType::kNone || ptt == TokenType::kOpenParenthesis;
       }
     } else if (ctt == TokenType::kOpenParenthesis) {
-      // An opening parenthesis should be preceded by an operator or another
-      // opening parenthesis.
-      res = ptt == TokenType::kNone || IsOperator(ptt) || IsFunction(ptt) ||
-            ptt == TokenType::kOpenParenthesis;
+      // An opening parenthesis should be preceded by any operator or another
+      // opening parenthesis or can also be the first char
+      // In other words by anything, except a value or a closing parenthesis
+      res = !IsValue(ptt) && ptt != TokenType::kCloseParenthesis;
     } else if (ctt == TokenType::kCloseParenthesis) {
-      // A closing parenthesis should be preceded by a number or another
-      // closing parenthesis.
+      // A closing parenthesis should be preceded by a number or another closing
+      // parenthesis.
       res = IsValue(ptt) || ptt == TokenType::kCloseParenthesis;
     } else if (IsFunction(ctt)) {
-      // A function should be preceded by a operator or an open parenthesis.
-      res = ptt == TokenType::kNone || IsOperator(ptt) ||
+      // A function should be preceded by a binary operator or an open
+      // parenthesis or can also be the first char
+      res = ptt == TokenType::kNone || IsBinaryOperator(ptt) ||
             ptt == TokenType::kOpenParenthesis;
     }
     return res;
