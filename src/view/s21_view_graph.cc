3d9b0b6df6f12b77e04ef4bc942dca3c79843c6a
elidacon <elidacon@student.21-school.ru>
2023-10-29 22:03:54 +0500

    Fix graphs output
    Fixes for cases like 1/x and changes to the step (h).

--- src/view/s21_view_graph.cc
+++ src/view/s21_view_graph.cc
@@ -17,15 +17,31 @@ void s21::ViewGraph::on_to_draw_button_clicked(double x_min, double x_max) {
   if (x_max > range_max) x_max = range_max;
   if (x_min < -range_max) x_min = -range_max;
 
-  double h = 0.1;
-  for (double X = x_min; X < x_max; X += h) {
-    x.push_back(X);
-    double Y = s21_controller_->Calculate(X);
-    y.push_back(Y);
+  double h = 0.1, y_value = 0;
+  if (x_max <= 100 && x_min >= -100) h = 0.01;
+  for (auto x_value = x_min; x_value < x_max; x_value += h) {
+    if (abs(x_value) < 1e-7) x_value = 0;
+    bool calculated = true;
+    try {
+      y_value = s21_controller_->Calculate(x_value);
+    } catch (const std::exception& e) {
+      calculated = false;
+    }
+    if (calculated && y_value <= range_max && y_value >= -range_max) {
+      y.push_back(y_value);
+      x.push_back(x_value);
+    } else {
+      // Add the previous segment to the graph
+      ui->widget->addGraph();
+      ui->widget->graph()->addData(x, y);
+      // Start a new segment
+      x.clear();
+      y.clear();
+    }
   }
 
   ui->widget->addGraph();
-  ui->widget->graph(0)->addData(x, y);
+  ui->widget->graph()->addData(x, y);
 
   ui->widget->setInteraction(QCP::iRangeZoom, true);
   ui->widget->setInteraction(QCP::iRangeDrag, true);
